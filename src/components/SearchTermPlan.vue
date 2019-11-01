060887d2f1a67e79ae1aafafae77ef0e72349aa2
nabarun.saha <nabarun.saha@cytel.com>
2019-11-01 19:52:52 +0530

    Completed functionality

---
+++ src/components/SearchTermPlan.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="col-md-9"></div>
+    <div class="pull-right">
+      <input
+        type="text"
+        v-model="plan"
+        placeholder="Search Plan"
+        @change="searchPlan"
+      />
+      <button @click="searchPlan">
+        <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchPlan",
+  data() {
+    return { plan: "" };
+  },
+  methods: {
+    searchPlan() {
+      this.$emit("search-plan", this.plan);
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/components/TermDetails.vue
+++ src/components/TermDetails.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Term InsurancePlans in India</h1>
+    <SearchPlan v-on:search-plan="searchplan" />
     <table class="table table-bordered">
       <thead class="table-primary">
         <tr>
@@ -13,13 +14,13 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="pd in planDetails" :key="pd.InsurerId">
+        <tr v-for="pd in filteredPlan" :key="pd.InsurerId">
           <td>{{ pd.Insurers }}</td>
           <td>{{ pd.TermPlans }}</td>
-          <td>{{ pd.MaximumMaturity }}</td>
-          <td>{{ pd.MinSumAssured }}</td>
-          <td>{{ pd.ClaimSettlement }}</td>
-          <td>{{ pd.PremiumPerMonth }}</td>
+          <td>{{ pd.MaximumMaturity | unit(" years") }}</td>
+          <td>{{ pd.MinSumAssured | amount(["Rs."]) }}</td>
+          <td>{{ pd.ClaimSettlement | unit("%") }}</td>
+          <td>{{ pd.PremiumPerMonth | amount(["Rs.", "/month"]) }}</td>
         </tr>
       </tbody>
     </table>
@@ -28,10 +29,32 @@
 
 <script>
 import axios from "axios";
+import SearchPlan from "./SearchTermPlan.vue";
 export default {
   name: "TermDetails",
+  components: {
+    SearchPlan
+  },
   data() {
-    return { planDetails: [] };
+    return { planDetails: [], filteredPlan: [] };
+  },
+  methods: {
+    searchplan(plan) {
+      console.log("In searchplan " + plan);
+      this.filteredPlan = plan
+        ? this.planDetails.filter(f => f.Insurers == plan)
+        : this.planDetails;
+    }
+  },
+  filters: {
+    unit(val, args) {
+      return `${val}${args}`;
+    },
+    amount(val, args) {
+      return args.length > 1
+        ? `${args[0]}${val}${args[1]}`
+        : `${args[0]}${val}`;
+    }
   },
   mounted() {
     axios
@@ -39,6 +62,7 @@ export default {
       .then(response => {
         // handle success
         this.planDetails = response.data;
+        this.filteredPlan = response.data;
         // eslint-disable-next-line
         // console.log(response.data);
       })
